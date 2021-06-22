fcb49d2a762e3d017dcc80ab55a7366c7cc850ba
woongchoi84 <woongchoi84@gmail.com>
2021-06-22 20:52:29 +0900

    comment

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,7 +8,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y full-upgrade
 
 sudo apt-get -y install xserver-xorg-input-all
 sudo apt-get -y build-essential
-sudo apt-get -y git
 sudo apt-get -y vim-gtk3
 
 cp .bash_aliases ~
