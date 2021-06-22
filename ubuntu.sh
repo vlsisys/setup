5d03a2ab5710c74649a960fdb19f692e7bf7b1b7
woongchoi84 <woongchoi84@gmail.com>
2021-06-22 21:00:35 +0900

    comment

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,15 +6,14 @@
 sudo apt-get -y update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y full-upgrade
 
-sudo apt-get -y install xserver-xorg-input-all
-sudo apt-get -y build-essential
-sudo apt-get -y vim-gtk3
+sudo apt-get -y install build-essential
+sudo apt-get -y install vim-gtk3
 
 cp .bash_aliases ~
-cp .vimrc
+cp .vimrc ~
 cp -rf bundle/ ~/.vim/
 
 sudo ubuntu-drivers autoinstall
-sudo reboot
+#sudo reboot
 
 
