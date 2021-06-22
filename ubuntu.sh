df7c11789002915895287300cfe543e7a8caf899
woongchoi84 <woongchoi84@gmail.com>
2021-06-22 21:08:34 +0900

    comment

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,6 +7,7 @@ sudo apt-get -y update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y full-upgrade
 
 sudo apt-get -y install build-essential
+sudo apt-get -y install samba
 sudo apt-get -y install vim-gtk3
 sudo apt-get -y install x11-apps dbus-x11
 sudo apt-get -y install gnome-terminal
@@ -28,6 +29,7 @@ sudo pip3 install torch torchvision
 cp .bash_aliases ~
 cp .vimrc ~
 cp -rf bundle/ ~/.vim/
+cp -rf sudo/ /usr/share/fonts/truetype/
 
 sudo ubuntu-drivers autoinstall
 #sudo reboot
