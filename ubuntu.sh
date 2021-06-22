7d1c5526790c4b85a9aea175692d45562bef2953
woongchoi84 <woongchoi84@gmail.com>
2021-06-22 21:13:14 +0900

    comment

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,7 +7,7 @@ sudo apt-get -y update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y full-upgrade
 
 sudo apt-get -y install build-essential
-sudo apt-get -y install samba
+sudo apt-get -y install samba openssh-server
 sudo apt-get -y install vim-gtk3
 sudo apt-get -y install x11-apps dbus-x11
 sudo apt-get -y install gnome-terminal
