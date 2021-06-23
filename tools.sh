ca9d986e272bcec3f9137d3047a49ebd34a9a250
vlsisys <woongchoi@sm.ac.kr>
2021-06-23 17:22:11 +0900

    comment

--- tools.sh
+++ tools.sh
@@ -3,22 +3,29 @@
 #  Coded by Woong
 # ==================================================
 
-# [cuda]
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-sudo apt-get update
-sudo apt-get -y install cuda
-
-# [TensorFlow]
-sudo pip3 install tensorflow
-
-# [Pytorch: should be checked official guideline depending on cudaDNN and OS version]
-sudo pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
-
-# [Samba]
-mkdir -p /vlsisys-samba
-chmod 777 /vlsisys-samba
-sudo ufw allow samba
-
+if [ `whoami`=="root" ]; then
+	# ----------------------------------------------
+	# [cuda]
+	# ----------------------------------------------
+	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+	sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+	sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+	sudo apt-get update
+	sudo apt-get -y install cuda
+	
+	# ----------------------------------------------
+	# [TensorFlow]
+	# ----------------------------------------------
+	sudo pip3 install tensorflow
+	
+	# ----------------------------------------------
+	# [Pytorch: should be checked official guideline depending on cudaDNN and OS version]
+	# ----------------------------------------------
+	sudo pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+	
+else
+	echo "======================================================================\n"
+	echo " This script is permitted for root!!!\n"
+	echo "======================================================================\n"
+fi
--- user.sh
+++ user.sh
@@ -1,9 +1,29 @@
 #! /bin/bash
+# ==================================================
+#  Coded by Woong
+# ==================================================
+
+# ----------------------------------------------
 # [Bash Environment Setup]
+# ----------------------------------------------
 cp .bash_aliases ~
 cp .vimrc ~
 mkdir -p ~/.vim
 mkdir -p ~/project
 cp -rf bundle ~/.vim/
 
-source ~/.bashrc
+echo "======================================================================\n"
+echo " To apply modification, please excute 'source ~/.bashrc'\n"
+echo "======================================================================\n"
+
+# ----------------------------------------------
+# [Jupyter Qt Console for Vim User - Optional]
+# ----------------------------------------------
+jupyter qtconsole --generate-config
+printf "\nc.ConsoleWidget.include_other_output = True\n" >> ~/.jupyter/jupyter_qtconsole_config.py
+
+echo "======================================================================\n"
+echo " For vim user, the following command can be used\n"
+echo "  $ jupyter qtconsole &\n"
+echo "  $ g <your_code>.py\n"
+echo "======================================================================\n"
