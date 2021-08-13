4f04d2b6392d790462e13ab1c0ffd5a9cf4276c7
vlsisys <woongchoi@sm.ac.kr>
2021-08-13 09:14:52 +0900

    comment

--- petalinux.sh
+++ petalinux.sh
@@ -5,12 +5,33 @@
 
 if [ `whoami` == "root" ]; then
 	# ----------------------------------------------
-	# [cuda]
+	# [Required Packages]
 	# ----------------------------------------------
 	sudo dpkg --add-architecture i386
-	sudo apt-get update
-	sudo apt-get -y install zlib1g:i386
-	sudo apt-get -y install net-tools xterm autoconf libtool texinfo gcc-multilib
+	sudo apt-get -y update
+	sudo apt-get -y install zlib1g:i386 net-tools xterm autoconf libtool texinfo gcc-multilib 
+
+	# ----------------------------------------------
+	# [TFTP Setting]
+	# ----------------------------------------------
+	sudo apt-get -y install tftp tftpd
+
+	sudo cp ./tftp /etc/xinetd.d/
+	sudo mkdir -p /tftpboot
+	sudo chmod 777 /tftpboot
+	sudo /etc/init.d/xinetd restart
+
+	# ----------------------------------------------
+	# [Shell: Dash -> Bash]
+	# ----------------------------------------------
+	sudo dpkg-reconfigure dash # Please Select No
+
+	echo "======================================================================\n"
+	echo " Next Step is as follows\n"
+	echo "======================================================================\n"
+	echo "	1.mkdir -p /opt/petalinux-v20xx.x \n"
+	echo "	2.download petalinux-v20xx.x.run \n"
+	echo "	3.$>./petalinux-v20xx.x.run --dir /opt/petalinux-v20xx.x\n"
 
 else
 	echo "======================================================================\n"
