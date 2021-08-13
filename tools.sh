1d5a950ca0c8ef126e44060280153688e846aecb
vlsisys <woongchoi@sm.ac.kr>
2021-08-13 14:41:46 +0900

    comment

--- tools.sh
+++ tools.sh
@@ -8,9 +8,9 @@ if [ `whoami` == "root" ]; then
 	# [cuda]
 	# ----------------------------------------------
 	if [[ $(lsb_release -rs) == "20.04" ]]; then
-		echo "======================================================================\n"
-		echo " Cuda Install for Ubuntu 20.04\n"
-		echo "======================================================================\n"
+		echo "======================================================================"
+		echo " Cuda Install for Ubuntu 20.04"
+		echo "======================================================================"
 		wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 		sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 		sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
@@ -18,9 +18,9 @@ if [ `whoami` == "root" ]; then
 		sudo apt-get update
 		sudo apt-get -y install cuda
 	elif [[ $(lsb_release -rs) == "18.04" ]]; then
-		echo "======================================================================\n"
-		echo " Cuda Install for Ubuntu 18.04\n"
-		echo "======================================================================\n"
+		echo "======================================================================"
+		echo " Cuda Install for Ubuntu 18.04"
+		echo "======================================================================"
 		wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 		sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 		sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
