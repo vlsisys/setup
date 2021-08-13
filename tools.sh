e31b15b908063e3645dd8c344ce0605ded2969b0
root <root@DESKTOP-5KT40F4.localdomain>
2021-08-13 14:28:40 +0900

    comment

--- tools.sh
+++ tools.sh
@@ -7,13 +7,26 @@ if [ `whoami` == "root" ]; then
 	# ----------------------------------------------
 	# [cuda]
 	# ----------------------------------------------
-	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-	sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-	sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-	sudo apt-get update
-	sudo apt-get -y install cuda
-	
+	if [[ $(lsb_release -rs) == "20.04" ]]; then
+		wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+		sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+		sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+		sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+		sudo apt-get update
+		sudo apt-get -y install cuda
+	elif [[ $(lsb_release -rs) == "18.04" ]]; then
+		wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+		sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+		sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+		sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+		sudo apt-get update
+		sudo apt-get -y install cuda
+	else
+		echo "======================================================================\n"
+		echo " Current Ubunutu can't support cuda install!!!\n"
+		echo "======================================================================\n"
+	fi
+
 	# ----------------------------------------------
 	# [TensorFlow]
 	# ----------------------------------------------
