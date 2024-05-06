fb2ee19c35198d58f73dff3e00d2a7251439bdb2
vlsisys <woongchoi@sm.ac.kr>
2024-05-06 16:44:40 +0900

    comment

---
+++ src/riscv_gnu_compiler_toolchain.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# --------------------------------------------------
+# [Basic Packages]
+# --------------------------------------------------
+echo '=============================='
+echo ' Install RISC-V GNU Compiler'
+echo '=============================='
+cd ~/utils/
+git clone https://github.com/riscv/riscv-gnu-toolchain
