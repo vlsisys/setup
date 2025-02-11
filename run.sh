ab3e52875d071ba140d0f791f2670f79a682deb0
vlsisys <woongchoi@sm.ac.kr>
2025-02-11 09:06:42 +0900

    comment

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 # --------------------------------------------------
 source	./src/advanced_packaging_tools.sh
 source	./src/shell.sh
-source	./src/riscv_gnu_compiler_toolchain.sh
-source	./src/gem5.sh
+#source	./src/riscv_gnu_compiler_toolchain.sh
+#source	./src/gem5.sh
 
 #source	./src/python.sh
 #source	./src/riscv-tools.sh
