618b8f4cca5f18f6b6f523c07526d0038968eebf
vlsisys <woongchoi@sookmyung.ac.kr>
2025-02-26 10:18:12 +0900

    comment

--- src/advanced_packaging_tools.sh
+++ src/advanced_packaging_tools.sh
@@ -13,7 +13,7 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install vim-gtk3
 sudo apt-get -y install x11-apps x11-server-utils dbus-x11
 sudo apt-get -y install gnome-terminal
-sudo apt-get -y install gawk tcl-dev tk-dev python3 python3-dev python3-pip python3-tk python3-git
+sudo apt-get -y install gawk tcl-dev tk-dev python3 python3-dev python3-pip python3-tk python3-git python3-tomli python3-venv
 sudo apt-get -y install cmake clang universal-ctags
 sudo apt-get -y install autoconf automake autotools-dev curl ninja-build git gdb
 sudo apt-get -y install bison flex texinfo gperf libtool patchutils bc zlib1g-dev
--- src/riscv_gnu_compiler_toolchain.sh
+++ src/riscv_gnu_compiler_toolchain.sh
@@ -10,6 +10,5 @@ mkdir -p ~/temp
 cd ~/temp/
 git clone https://github.com/riscv/riscv-gnu-toolchain
 cd riscv-gnu-toolchain
-mkdir -p build; cd build
-../configure --prefix=$HOME/utils/riscv --enable-multilib
+./configure --prefix=$HOME/tools/riscv --enable-multilib
 make linux
