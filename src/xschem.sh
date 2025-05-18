1ebcbbfb604292b5e72ffe04ee60e8f426a80a3c
vlsisys <woongchoi@sm.ac.kr>
2025-05-18 19:25:14 +0900

    comment

--- src/xschem.sh
+++ src/xschem.sh
@@ -16,3 +16,22 @@ sudo apt-get -y  install libjpeg-dev
 # --------------------------------------------------
 git clone https://github.com/StefanSchippers/xschem.git ~/tools/xschem-src
 cd ~/tools/xschem-src; ./configure
+sudo make install
+
+# --------------------------------------------------
+# [Magic: Layout Editor Install]
+# --------------------------------------------------
+#git clone git://opencircuitdesign.com/magic
+sudo apt-get -y install magic
+
+# --------------------------------------------------
+# Installing Open_PDKs version 1.0 for Google/SkyWater
+# --------------------------------------------------
+git clone git://opencircuitdesign.com/open_pdks ~/tools/open_pdks
+cd ~/tools/open_pdks
+./configure --enable-sky130-pdk --enable-sram-sky130
+make
+sudo make install
+#make veryclean
+
+echo 'source /usr/local/share/pdk/sky130B/libs.tech/xschem/xschemrc' > ~/.xschem/xschemrc
